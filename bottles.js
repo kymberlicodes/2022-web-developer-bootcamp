dd751c2b2b0a77e77ca5a983e69b0345199bcb97
Kymberli <kymberli@honeycrm.com>
2022-02-19 21:41:20 -0600

    Wrote a program that 'sings' 99 bottles of beer on the wall for intermediate javascript module.

--- bottles.js
+++ bottles.js
@@ -2,10 +2,10 @@ var totalBottles = 99;
 
 function sing(bottles) {
 	if (bottles > 0) {
-    console.log(`${bottles} ${bottles > 1 ? 'bottles' : 'bottle'} of beer on the wall, ${bottles} ${bottles > 1 ? 'bottles' : 'bottle'} of beer.\nTake one down, and pass it around, ${bottles-1} ${bottles-1 > 1 || bottles-1 === 0 ? 'bottles' : 'bottle'} of beer on the wall.`);
-  } else {
-  	console.log(`No more bottles of beer on the wall, no more bottles of beer.\nGo to the store, buy some more. 99 bottles of beer on the wall.`);
-  }
+        console.log(`${bottles} ${bottles > 1 ? 'bottles' : 'bottle'} of beer on the wall, ${bottles} ${bottles > 1 ? 'bottles' : 'bottle'} of beer.\nTake one down, and pass it around, ${bottles-1} ${bottles-1 > 1 || bottles-1 === 0 ? 'bottles' : 'bottle'} of beer on the wall.`);
+    } else {
+        console.log(`No more bottles of beer on the wall, no more bottles of beer.\nGo to the store, buy some more. 99 bottles of beer on the wall.`);
+    }
 }
 
 while (totalBottles >= 0) {
